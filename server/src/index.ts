5b9864342ab4ddd8812f0582d672a227f9a68efb
azagatti <andre.zagatti1@gmail.com>
2020-09-14 22:11:17 -0300

    feat: setup graphql

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,34 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+
 import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
-  const orm = await MikroORM.init({
-    dbName: "lireddit",
-    user: "postgres",
-    password: "docker",
-    debug: !__prod__,
-    type: "postgresql",
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
   });
-  console.log("Hello World");
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started");
+  });
 };
 
-main();
+main().catch((err) => console.error(err));
