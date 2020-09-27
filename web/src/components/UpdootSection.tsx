392e31e2670f0d11f148637fb755f00e69ef79ee
azagatti <andre.zagatti1@gmail.com>
2020-09-26 23:25:22 -0300

    feat: create frontend to up and down vote

---
+++ web/src/components/UpdootSection.tsx
@@ -0,0 +1,59 @@
+import { Flex, IconButton } from "@chakra-ui/core";
+import React, { useState } from "react";
+
+import { PostSnippetFragment, useVoteMutation } from "../generated/graphql";
+
+interface UpdootSectionProps {
+  post: PostSnippetFragment;
+}
+
+const UpdootSection: React.FC<UpdootSectionProps> = ({ post }) => {
+  const [loadingState, setLoadingState] = useState<
+    "updoot-loading" | "downdoot-loading" | "not-loading"
+  >("not-loading");
+  const [, handleVote] = useVoteMutation();
+
+  return (
+    <Flex direction="column" alignItems="center" justifyContent="center" mr={4}>
+      <IconButton
+        onClick={async () => {
+          if (post.voteStatus === 1) {
+            return;
+          }
+          setLoadingState("updoot-loading");
+          await handleVote({
+            postId: post.id,
+            value: 1,
+          });
+          setLoadingState("not-loading");
+        }}
+        variantColor={post.voteStatus === 1 ? "green" : undefined}
+        isLoading={loadingState === "updoot-loading"}
+        icon="chevron-up"
+        aria-label="Up"
+        fontSize="24px"
+      />
+      {post.points}
+      <IconButton
+        onClick={async () => {
+          if (post.voteStatus === -1) {
+            return;
+          }
+          setLoadingState("downdoot-loading");
+          await handleVote({
+            postId: post.id,
+            value: -1,
+          });
+          setLoadingState("not-loading");
+        }}
+        variantColor={post.voteStatus === -1 ? "red" : undefined}
+        isLoading={loadingState === "downdoot-loading"}
+        icon="chevron-down"
+        aria-label="Down"
+        fontSize="24px"
+      />
+    </Flex>
+  );
+};
+
+export default UpdootSection;
